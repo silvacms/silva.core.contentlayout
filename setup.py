aa9f573f06491273572c300edb18b17adc5f2c71
Andy Altepeter <aaltepet@altepeter.net>
2011-01-20 14:11:42 -0600

    initial

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='silva.core.contentlayout',
+      version=version,
+      description="content layout base functionality",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Andrew Altepeter',
+      author_email='aaltepet@bethel.edu',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva', 'silva.core'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
