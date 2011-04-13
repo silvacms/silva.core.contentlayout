3a128e4b6e3dfe316345a1cab9d8e5ce36144887
Andy Altepeter <aaltepet@altepeter.net>
2011-04-13 14:24:49 -0500

    adjust grammar

--- silva/core/contentlayout/templates/default.py
+++ silva/core/contentlayout/templates/default.py
@@ -8,13 +8,13 @@ grok.layer(ISilvaLayer)
 class DefaultContentTemplate(grok.View):
     """The default content template for rendered Silva content which does not
        support IContentLayout.  This view wraps the rendered silva content 
-       (passed in via set_content) with the desires default content template.
+       (passed in via set_content) with the desired default content template.
        The dct can add a left nav (or not), a title (at the appropriate heading
        level), etc.
        
        Override this in your own layer to provide a custom-tailored default
        content template for your own layout.  You can also create layouts
-       tailored for specific content types by changing the grok.context.
+       tailored for specific content types by changing grok.context.
     """
     grok.context(ISilvaObject)
     grok.implements(IDefaultContentTemplate)
